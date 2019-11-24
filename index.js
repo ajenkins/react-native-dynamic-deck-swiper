de4ca408f4cdf245de4c23fc0b459c17bd5f0441
AJ Jenkins <aj.jenkins123@gmail.com>
2019-11-24 18:25:58 -0500

    Added top-level component

---
+++ index.js
@@ -0,0 +1,3 @@
+import MyComponent from './src/swiper';
+
+export default MyComponent;
---
+++ src/swiper.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const MyComponent = () => <Text>Hello world!</Text>;
+
+export default MyComponent;
