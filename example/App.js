3e35ada36691256c7c9e3856601f412702f28749
AJ Jenkins <aj.jenkins123@gmail.com>
2019-11-24 18:40:29 -0500

    Successfully import my component into expo app

--- example/App.js
+++ example/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MyComponent from 'react-native-dynamic-deck-swiper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyComponent />
     </View>
   );
 }
@@ -14,6 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
