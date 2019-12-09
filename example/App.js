95ccdbe8402050c893562ff84d4837ad9df89654
AJ Jenkins <aj.jenkins123@gmail.com>
2019-12-08 21:14:12 -0500

    Removed unused import

--- example/App.js
+++ example/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Swiper from 'react-native-dynamic-deck-swiper';
 
