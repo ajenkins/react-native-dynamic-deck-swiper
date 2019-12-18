f71991e147db7f1a839da38847b7df7e96e8df8f
AJ Jenkins <aj.jenkins123@gmail.com>
2019-12-17 21:09:36 -0500

    Fixed bug with how I was calculating the current swipe direction

--- src/panresponder.js
+++ src/panresponder.js
@@ -6,6 +6,48 @@ import { LEFT, RIGHT, UP, DOWN } from './swiper';
 // https://facebook.github.io/react-native/docs/dimensions#get
 const { height, width } = Dimensions.get('window');
 
+/**
+ * Get the swipe direction based on the current offset of the top card.
+ * Will return the direction that the card is currently pulled the
+ * farthest in (most extreme) relative to the height and width of the
+ * screen.
+ * @param {Number} x
+ * @param {Number} y
+ * @returns {String, null} - A swipe direction or null
+ */
+const getSwipeDirection = (
+  x,
+  y,
+  disableUp,
+  disableDown,
+  disableLeft,
+  disableRight
+) => {
+  const xAdj = x / width;
+  const yAdj = y / height;
+  let potentialDirections = [];
+  if (x > 0 && !disableRight) {
+    potentialDirections.push({ offset: Math.abs(xAdj), dir: RIGHT });
+  }
+  if (x < 0 && !disableLeft) {
+    potentialDirections.push({ offset: Math.abs(xAdj), dir: LEFT });
+  }
+  if (y > 0 && !disableDown) {
+    potentialDirections.push({ offset: Math.abs(yAdj), dir: DOWN });
+  }
+  if (y < 0 && !disableUp) {
+    potentialDirections.push({ offset: Math.abs(yAdj), dir: UP });
+  }
+  if (potentialDirections.length) {
+    const sortedDirections = potentialDirections.sort(
+      (a, b) => a.offset - b.offset
+    );
+    return sortedDirections[0].dir;
+  } else {
+    return null;
+  }
+};
+
 const onPanResponderMove = (gestureState, position) => (state, props) => {
   // Move card
   if (props.preventVerticalDragging) {
@@ -15,19 +57,15 @@ const onPanResponderMove = (gestureState, position) => (state, props) => {
   }
 
   // Update state based on current card location
-  if (Math.abs(gestureState.dx) / width >= Math.abs(gestureState.dy) / height) {
-    if (gestureState.dx > 0) {
-      return props.disableSwipeRight ? {} : { swipeDirection: RIGHT };
-    } else if (gestureState.dx < 0) {
-      return props.disableSwipeLeft ? {} : { swipeDirection: LEFT };
-    }
-  } else {
-    if (gestureState.dy > 0) {
-      return props.disableSwipeDown ? {} : { swipeDirection: DOWN };
-    } else if (gestureState.dy < 0) {
-      return props.disableSwipeUp ? {} : { swipeDirection: UP };
-    }
-  }
+  const swipeDirection = getSwipeDirection(
+    gestureState.dx,
+    gestureState.dy,
+    props.disableSwipeUp,
+    props.disableSwipeDown,
+    props.disableSwipeLeft,
+    props.disableSwipeRight
+  );
+  return { swipeDirection };
 };
 
 const onPanResponderRelease = (gestureState, position, _getNextCardData) => (
